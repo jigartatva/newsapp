a6f0ca46dd5e2b9d3034652a3c7ee12244c38020
Denish Makadiya <denish.makadiya@tatvasoft.com>
2018-10-05 17:40:51 +0530

    Set default configuration.

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 
 const apiEnvironment = {
   development: {
-    api: process.env.DEV_API_ENDPOINT || '',
-    key: process.env.DEV_API_KEY || '',
+    api: process.env.DEV_API_ENDPOINT || 'https://newsapi.org/v2',
+    key: process.env.DEV_API_KEY || '0d868cdbccbe40d29f4a1e6a94f5c155',
   },
   production: {
-    api: process.env.PROD_API_ENDPOINT || '',
-    key: process.env.PROD_API_KEY || '',
+    api: process.env.PROD_API_ENDPOINT || 'https://newsapi.org/v2',
+    key: process.env.PROD_API_KEY || '0d868cdbccbe40d29f4a1e6a94f5c155',
   }
 };
 
